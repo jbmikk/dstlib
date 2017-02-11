76ce0ef10a9329b4a3e16f65318fd901a5b07e97
Jens Mikkelsen <jmikkelsen@frba.utn.edu.ar>
2017-02-11 19:17:34 -0300

    Fix indentation

--- src/structs.h
+++ src/structs.h
@@ -2,28 +2,28 @@
 #define CSTRUCT
 
 typedef struct _Node {
-    struct _Node *child;
-    char type;
-    unsigned char size;
-    unsigned char key;
-    void *array;
-    void *data;
+	struct _Node *child;
+	char type;
+	unsigned char size;
+	unsigned char key;
+	void *array;
+	void *data;
 } Node;
 
 typedef struct _Iterator {
-    Node *root;
-    void *key;
-    unsigned char size;
-    void *data;
+	Node *root;
+	void *key;
+	unsigned char size;
+	void *data;
 } Iterator;
 
 typedef struct _ScanStatus {
-    void *key;
-    unsigned char size;
-    unsigned int index;
-    unsigned int subindex;
-    unsigned int type;
-    unsigned int found;
+	void *key;
+	unsigned char size;
+	unsigned int index;
+	unsigned int subindex;
+	unsigned int type;
+	unsigned int found;
 } ScanStatus;
 
 typedef struct _SNode {
