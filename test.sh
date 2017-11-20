0016485a7180e09f8dd5a6f1634abcfad3fcb52f
Jens Mikkelsen <jmikkelsen@frba.utn.edu.ar>
2017-11-20 14:08:03 -0300

    Rename test files

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ echo ""
 echo "TESTSTLIB:"
 echo "---------"
 
-run_test $MODE $PATTERN ./test_stack
-run_test $MODE $PATTERN ./test_bsearch
-run_test $MODE $PATTERN ./test_radixtree
-run_test $MODE $PATTERN ./test_arrays
+run_test $MODE $PATTERN ./stack_test
+run_test $MODE $PATTERN ./bsearch_test
+run_test $MODE $PATTERN ./radixtree_test
+run_test $MODE $PATTERN ./arrays_test
