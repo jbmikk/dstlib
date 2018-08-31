c69cb798cf444f9162d42e5a96c23b565d7f3071
Jens Mikkelsen <jmikkelsen@frba.utn.edu.ar>
2018-08-31 00:33:51 -0300

    Add basic append bmap function

--- tests/bmap_test.c
+++ tests/bmap_test.c
@@ -246,6 +246,122 @@ void bmap__set3_and_delete1(){
 	t_assert(c2->btest.data == (void *)&d3);
 }
 
+void bmap__append_key_middle(){
+
+	BMapEntryBTest *b1, *b2, *b3;
+	int dummy1 = 0, dummy2 = 0, dummy3 = 0;
+
+	bmap_btest_insert(&fixture.bmap, 10, (BTest){ &dummy1 });
+	bmap_btest_insert(&fixture.bmap, 128, (BTest){ &dummy2 });
+	bmap_btest_append(&fixture.bmap, 10, (BTest){ &dummy3 });
+
+	t_assert(bmap_btest_count(&fixture.bmap) == 3);
+	t_assert(bmap_btest_first(&fixture.bmap)->key == 10);
+	BMapCursorBTest cur;
+
+	bmap_cursor_btest_init(&cur, &fixture.bmap);
+	bool first = bmap_cursor_btest_next(&cur);
+	b1 = bmap_cursor_btest_current(&cur);
+
+	bool second = bmap_cursor_btest_next(&cur);
+	b2 = bmap_cursor_btest_current(&cur);
+
+	bool third = bmap_cursor_btest_next(&cur);
+	b3 = bmap_cursor_btest_current(&cur);
+
+	bool fourth = bmap_cursor_btest_next(&cur);
+
+	bmap_cursor_btest_dispose(&cur);
+
+	t_assert(first);
+	t_assert(b1 != NULL);
+	t_assert(b1->btest.data == (void *)&dummy1);
+
+	t_assert(second);
+	t_assert(b2 != NULL);
+	t_assert(b2->btest.data == (void *)&dummy3);
+
+	t_assert(third);
+	t_assert(b3 != NULL);
+	t_assert(b3->btest.data == (void *)&dummy2);
+	t_assert(!fourth);
+}
+
+void bmap__append_key_last(){
+
+	BMapEntryBTest *b1, *b2, *b3;
+	int dummy1 = 0, dummy2 = 0, dummy3 = 0;
+
+	bmap_btest_insert(&fixture.bmap, 10, (BTest){ &dummy1 });
+	bmap_btest_insert(&fixture.bmap, 128, (BTest){ &dummy2 });
+	bmap_btest_append(&fixture.bmap, 128, (BTest){ &dummy3 });
+
+	t_assert(bmap_btest_count(&fixture.bmap) == 3);
+	t_assert(bmap_btest_first(&fixture.bmap)->key == 10);
+	BMapCursorBTest cur;
+
+	bmap_cursor_btest_init(&cur, &fixture.bmap);
+	bool first = bmap_cursor_btest_next(&cur);
+	b1 = bmap_cursor_btest_current(&cur);
+
+	bool second = bmap_cursor_btest_next(&cur);
+	b2 = bmap_cursor_btest_current(&cur);
+
+	bool third = bmap_cursor_btest_next(&cur);
+	b3 = bmap_cursor_btest_current(&cur);
+
+	bool fourth = bmap_cursor_btest_next(&cur);
+
+	bmap_cursor_btest_dispose(&cur);
+
+	t_assert(first);
+	t_assert(b1 != NULL);
+	t_assert(b1->btest.data == (void *)&dummy1);
+
+	t_assert(second);
+	t_assert(b2 != NULL);
+	t_assert(b2->btest.data == (void *)&dummy2);
+
+	t_assert(third);
+	t_assert(b3 != NULL);
+	t_assert(b3->btest.data == (void *)&dummy3);
+	t_assert(!fourth);
+}
+
+void bmap__append_key_single(){
+
+	BMapEntryBTest *b1, *b2;
+	int dummy1 = 0, dummy2 = 0;
+
+	bmap_btest_insert(&fixture.bmap, 10, (BTest){ &dummy1 });
+	bmap_btest_append(&fixture.bmap, 128, (BTest){ &dummy2 });
+
+	t_assert(bmap_btest_count(&fixture.bmap) == 2);
+	t_assert(bmap_btest_first(&fixture.bmap)->key == 10);
+	BMapCursorBTest cur;
+
+	bmap_cursor_btest_init(&cur, &fixture.bmap);
+	bool first = bmap_cursor_btest_next(&cur);
+	b1 = bmap_cursor_btest_current(&cur);
+
+	bool second = bmap_cursor_btest_next(&cur);
+	b2 = bmap_cursor_btest_current(&cur);
+
+	bool third = bmap_cursor_btest_next(&cur);
+
+	bmap_cursor_btest_dispose(&cur);
+
+	t_assert(first);
+	t_assert(b1 != NULL);
+	t_assert(b1->btest.data == (void *)&dummy1);
+
+	t_assert(second);
+	t_assert(b2 != NULL);
+	t_assert(b2->btest.data == (void *)&dummy2);
+
+	t_assert(!third);
+}
+
 void bmap_cursor_btest__iterate_zero(){
 	BMapCursorBTest cur;
 
@@ -485,6 +601,9 @@ int main(int argc, char** argv) {
 	t_test(bmap__set2_and_get2_ensure_unsigned);
 	t_test(bmap__set2_and_delete1);
 	t_test(bmap__set3_and_delete1);
+	t_test(bmap__append_key_middle);
+	t_test(bmap__append_key_last);
+	t_test(bmap__append_key_single);
 	t_test(bmap_cursor_btest__iterate_zero);
 	t_test(bmap_cursor_btest__iterate_one);
 	t_test(bmap_cursor_btest__iterate_two);
